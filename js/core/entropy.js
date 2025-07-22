daf591c9bb1c39ff945dcc1c72e191395f25130f
damindu817 <damindu817@gmail.com>
2025-07-22 15:13:38 +0530

    Update entropy.js

--- js/core/entropy.js
+++ js/core/entropy.js
@@ -1,1 +1,9 @@
-
+// Calculate entropy of a SHA512 hash
+function calcEntropy(hash) {
+  const freq = {};
+  for (let c of hash) freq[c] = (freq[c] || 0) + 1;
+  return Object.values(freq).reduce((acc, f) => {
+    const p = f / hash.length;
+    return acc - p * Math.log2(p);
+  }, 0);
+}
