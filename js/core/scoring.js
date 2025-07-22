0faa9d9cb0967030ae804ce955b0baa5f4f140c3
damindu817 <damindu817@gmail.com>
2025-07-22 15:13:55 +0530

    Update scoring.js

--- js/core/scoring.js
+++ js/core/scoring.js
@@ -1,1 +1,5 @@
-
+// Calculate score from hash based on specific positions
+function scoreFromHash(hash) {
+  const indexes = [5, 15, 25, 35, 50, 75, 100, 120];
+  return indexes.reduce((sum, i) => sum + parseInt(hash[i], 16), 0);
+}
